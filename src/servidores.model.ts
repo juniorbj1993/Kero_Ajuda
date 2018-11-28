5a9196771243c87286a85a99c447298b6c87bb2c
jbjunior <juniorbj1993@gmail.com>
2018-11-28 13:37:57 -0300

    atualizado

--- src/servidores.model.ts
+++ src/servidores.model.ts
@@ -1,10 +1,14 @@
 export class Servidores{
+  public id:number;
   public img:string;
   public nome:string;
   public telefone: string;
   public email: string;
   public categoria: string;
   public funcao:string;
+  public cpf:string;
+  public rg:string;
+
 
   public estado: string;
   public cidade: string;
@@ -13,8 +17,13 @@ export class Servidores{
   public numero:string;
 
   public curriculos:string;
-
   public estrelas:Array<number>;
+  public nome_pai:string;
+  public nome_mae:string;
+  public estado_civil:string;
+  public datadenascimento:string;
+  public nacionalidade:string;
+  public naturalidade:string;
 
 
 }
