9e70824d0bd8e55d18c93579d94ed46fda158749
jbjunior <juniorbj1993@gmail.com>
2019-03-01 07:39:42 -0300

    adicionado search

--- src/pages/pdsdados/listapds/listapds.ts
+++ src/pages/pdsdados/listapds/listapds.ts
@@ -13,9 +13,13 @@ import { DetalhepdsPage } from '../detalhepds/detalhepds';
 export class ListapdsPage {
   servico: string
   pds_dados: Array<object>
-  constructor(public navCtrl: NavController, 
+  dadosfiltrados: Array<object>;
+  searchQueryCidade = '';
+  searchQueryBairro = '';
+
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    
+
     ) {
   }
 
@@ -23,16 +27,50 @@ export class ListapdsPage {
     if(this.navParams.get('dados') && this.navParams.get('dados_pds')){
       this.servico = this.navParams.get('dados');
       this.pds_dados = this.navParams.get('dados_pds');
+      this.initializeItems();
     }
     else{
       this.navCtrl.pop();
     }
   }
-  
+
   detalharPds(pds: pds){
     this.navCtrl.push(DetalhepdsPage,{
       dadospds: pds
     })
   }
+  initializeItems() {
+    this.dadosfiltrados = this.pds_dados;
+
+  }
+  searchBairro(ev){
+
+    // set val to the value of the ev target
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.dadosfiltrados = this.dadosfiltrados.filter((item) => {
+        return (item.bairro.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  searchCidade(ev){
+      // set val to the value of the ev target
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.dadosfiltrados = this.dadosfiltrados.filter((item) => {
+        return (item.cidade.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  resetarPesquisa(){
+    this.initializeItems();
+    this.searchQueryCidade = '';
+    this.searchQueryBairro = '';
+  }
+
 
 }
